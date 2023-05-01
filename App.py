76fa3c368d8ff7549ae75797141c0f27c348d323
piljocom <132269874+piljocom@users.noreply.github.com>
2023-05-01 21:26:58 +0900

    Add files via upload

---
+++ App.py
@@ -0,0 +1,28 @@
+import openpyxl
+import ping3
+import streamlit as st
+
+def ping_ping3(hostname):
+    timeout = 3
+    response = ping3.ping(hostname, timeout=timeout)
+    if response == None:
+        result = "Fail"
+    else:
+        result = "Success"
+    return result
+
+ 
+filename = "test.xlsx"
+wb = openpyxl.load_workbook(filename)
+ws = wb.active
+st.write('# Ping Try')
+for x in range(1, ws.max_row + 1):
+    hostname = ws.cell(row=x, column=2).value
+    print(hostname, end="\n")
+    ping_result = ping_ping3(hostname)
+    print(ping_result)
+    ws.cell(row=x, column=1).value = ping_result
+    hostname
+    ping_result
+
+wb.save(filename) # 파일 저장
